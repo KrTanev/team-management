f4af2184da4d53740beccbecf2ea702a722874a9
petyr.lubomirov.petrov@gmail.com <petyr.lubomirov.petrov@gmail.com>
2025-12-03 16:00:24 +0200

    Added - AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@emotion/react";
 import { createTheme, CssBaseline } from "@mui/material";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./config/queryClient.config";
+import { AuthProvider } from "./utils/providers/AuthProvider";
 
 const router = createBrowserRouter(routes);
 
@@ -11,12 +12,14 @@ function App() {
   const theme = createTheme();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </AuthProvider>
   );
 }
 
